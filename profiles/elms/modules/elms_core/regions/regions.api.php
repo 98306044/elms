53db9d2cc22b7539074a2aaee2ec1b1680a60c5d
Bryan Ollendyke <bto108@psu.edu>
2011-09-23 14:21:04 -0400

    Regions API documented

---
+++ profiles/elms/modules/elms_core/regions/regions.api.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by Regions.
+ */
+
+/**
+ * Add regions to be created
+ *
+ * Allows other modules to define regions
+ *
+ */
+function hook_define_regions() {
+  //this could also be module_name if you want to be consistent
+  $regions['my_region_1'] = array(
+    //required: name of the module invoking this
+    'project' => 'module_name',
+	//optional: css file name in the project folder, no leading slash
+	'css' => 'style.css',
+	//optional: js file name in the project folder, no leading slash
+	'js' => 'script.js'
+  );
+  //you can define as many regions as you want though including only 1 is more modular
+  $regions['my_region_2'] = array(
+    'project' => 'module_name', 
+	'css' => 'style.css',
+	'js' => 'script.js'
+  );
+  return $regions;
+}
