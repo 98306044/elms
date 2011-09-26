53e10d2e7a42b8470c7a988472943f3dc0dae451
Bryan Ollendyke <bto108@psu.edu>
2011-09-26 16:47:57 -0400

    converted elms_nav_top module to a feature

--- profiles/elms/modules/elms_core/elms_nav_top/elms_nav_top.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-  $('#regions_block_elms_nav_top_2 .regions_block_title').click(function(){
-    $('#regions_block_elms_nav_top_2 .regions_block_content').slideToggle(500);
-  });
-});
