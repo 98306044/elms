9e258c1eb28f3d0b5b0b2b3b9e8faf366da8d754
Bryan Ollendyke <bto108@psu.edu>
2011-09-26 13:35:29 -0400

    conversion over to hijacked menu title

--- profiles/elms/modules/elms_core/elms_nav_top/elms_nav_top.js
+++ profiles/elms/modules/elms_core/elms_nav_top/elms_nav_top.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $('#regions_block_menu_menu-usermenu .regions_block_title').click(function(){
-    $('#regions_block_menu_menu-usermenu .regions_block_content').slideToggle(500);
+  $('#regions_block_elms_nav_top_2 .regions_block_title').click(function(){
+    $('#regions_block_elms_nav_top_2 .regions_block_content').slideToggle(500);
   });
 });
