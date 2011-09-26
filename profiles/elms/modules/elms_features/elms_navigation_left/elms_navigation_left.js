a159c42865ac082d0edb898e67f0702f153221f5
Bryan Ollendyke <bto108@psu.edu>
2011-09-26 17:02:09 -0400

    moved elms_nav_left to a feature as well

---
+++ profiles/elms/modules/elms_features/elms_navigation_left/elms_navigation_left.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  //establish the default state for the nav which is closed
+  $('#regions_elms_navigation_left .regions_pre_block_container').addClass('regions_toggle_closed');
+  //when clicking the title, bring it to the front
+  $('#regions_elms_navigation_left .regions_pre_block_container').click(function(){
+    //open this region if clicked on a tab, close if you do
+	if ($('#regions_elms_navigation_left .regions_pre_block_container').hasClass('regions_toggle_closed')) {
+	  $('#regions_elms_navigation_left .regions_pre_block_container').addClass('regions_toggle_open');
+	  $('#regions_elms_navigation_left .regions_pre_block_container').removeClass('regions_toggle_closed');
+	  $('#regions_elms_navigation_left').animate({left:'0'}, 500);
+	}
+	else {
+	  $('#regions_elms_navigation_left .regions_pre_block_container').addClass('regions_toggle_closed');
+	  $('#regions_elms_navigation_left .regions_pre_block_container').removeClass('regions_toggle_open');
+	  $('#regions_elms_navigation_left').animate({left:'-200'}, 500);
+	}
+  });
+  //collapse based on toggle
+  $('#regions_elms_navigation_left .regions_toggle').click(function(){
+	//additional logic is to account for menu being opened by clicking a block title
+	if ($(this).hasClass('regions_toggle_closed')) {
+	  $(this).addClass('regions_toggle_open');
+	  $(this).removeClass('regions_toggle_closed');
+	  $('#regions_elms_navigation_left').animate({left:'0'}, 500);
+	}
+	else {
+	  $(this).addClass('regions_toggle_closed');
+	  $(this).removeClass('regions_toggle_open');
+	  $('#regions_elms_navigation_left').animate({left:'-200'}, 500);
+	}
+  });
+});
