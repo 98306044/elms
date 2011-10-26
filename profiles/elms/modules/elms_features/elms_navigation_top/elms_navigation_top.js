ae4028ffdcb4eb430f132e937f704bb1e2104a0b
Bryan Ollendyke <bto108@psu.edu>
2011-10-26 11:05:09 -0400

    webos style status messages check!

--- profiles/elms/modules/elms_features/elms_navigation_top/elms_navigation_top.js
+++ profiles/elms/modules/elms_features/elms_navigation_top/elms_navigation_top.js
@@ -3,6 +3,16 @@ $(document).ready(function(){
   $('#regions_block_elms_navigation_top_2 .regions_block_title').click(function(){
     $('#regions_block_elms_navigation_top_2 .regions_block_content').slideToggle('fast', 'linear');
   });
+  $('#regions_block_elms_navigation_top_1 .regions_block_title').click(function(){
+    $('#regions_block_elms_navigation_top_1 .regions_block_content').slideToggle('fast', 'linear');
+    //class to retain the hover state
+    if ($(this).hasClass('elms_nav_top_open')) {
+      $(this).removeClass('elms_nav_top_open');
+    }
+    else {
+      $(this).addClass('elms_nav_top_open');
+    }
+  });
   //scaling capabilities
   $('#regions_block_elms_navigation_top_3 .elms_zoom_in').click(function(){
     if (Drupal.settings.elms_navigation_top.factor <= 1.5) {
@@ -17,7 +27,7 @@ $(document).ready(function(){
     }
   });
   $('#regions_block_elms_navigation_top_3 .elms_zoom_reset').click(function(){
-	  Drupal.settings.elms_navigation_top.factor = 1;
-	  $('body').css('zoom', Drupal.settings.elms_navigation_top.factor).css('-moz-transform', 'scale('+ Drupal.settings.elms_navigation_top.factor +')');
+      Drupal.settings.elms_navigation_top.factor = 1;
+      $('body').css('zoom', Drupal.settings.elms_navigation_top.factor).css('-moz-transform', 'scale('+ Drupal.settings.elms_navigation_top.factor +')');
   });
 });
