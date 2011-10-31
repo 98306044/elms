a5ff70141acf8cfb1ad7ce218baa8dc680f28a12
Bryan Ollendyke <bto108@psu.edu>
2011-10-31 09:02:41 -0400

    moving to install profile

---
+++ profiles/elms/libraries/ckeditor/plugins/elimedia/plugin.js
@@ -0,0 +1,25 @@
+﻿/*********************************************************************************************************/
+/**
+ * elimedia plugin for CKEditor 3.x (Author: Lajox ; Email: lajox@19www.com)
+ * version:	 1.0
+ * Released: On 2009-12-11
+ * Download: http://code.google.com/p/lajox
+ */
+/*********************************************************************************************************/
+
+CKEDITOR.plugins.add('elimedia',   
+  {    
+    requires: ['dialog'],
+		lang : ['en'], 
+    init:function(a) { 
+	var b="elimedia";
+	var c=a.addCommand(b,new CKEDITOR.dialogCommand(b));
+		c.modes={wysiwyg:1,source:0};
+		c.canUndo=false;
+	a.ui.addButton("elimedia",{
+					label:a.lang.elimedia.title,
+					command:b,
+					icon:this.path+"elimedia.png"
+	});
+	CKEDITOR.dialog.add(b,this.path+"dialogs/elimedia.js")}
+});
---
+++ profiles/elms/libraries/ckeditor/plugins/elimedia_webcam/lang/en.js
@@ -0,0 +1,8 @@
+﻿CKEDITOR.plugins.setLang("elimedia_webcam","en",{
+  elimedia_webcam:
+    {
+	 title:'ELIMedia Webcam',
+	 commonTab:'Info',
+	 HelpInfo:'Insert a live video from your webcam'
+	}
+});
