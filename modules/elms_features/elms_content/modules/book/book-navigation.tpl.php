296f2604422cfb25ea4d7fcc898a55ba181ad3c3
Bryan Ollendyke <bto108@psu.edu>
2012-02-20 14:48:34 -0500

    coder tough love

--- modules/elms_features/elms_content/modules/book/book-navigation.tpl.php
+++ modules/elms_features/elms_content/modules/book/book-navigation.tpl.php
@@ -1,3 +1,8 @@
-<?php 
-//this file exists because a lot of themes don't style the book-navigation.tpl.php file. as a result it will want to look in the modules/book folder (which we've made relative to the module at this point)
+<?php
+/**
+ * @file
+ * This is incase a path should reference this file
+ *
+ * Point to the file included lower
+ */
   include_once dirname(__FILE__) .'/../../book-navigation.tpl.php';
