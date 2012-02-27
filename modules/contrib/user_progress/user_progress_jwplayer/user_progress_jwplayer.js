6acc2a671350029da4516040abd08d84026852b7
Bryan Ollendyke <bto108@psu.edu>
2012-02-27 09:38:48 -0500

    more logical tracking
    also added a commented out example that will confirm every 0.1 second
    they have watched the video, round it and send it to the progress
    table...you know... for overkill

--- modules/contrib/user_progress/user_progress_jwplayer/user_progress_jwplayer.js
+++ modules/contrib/user_progress/user_progress_jwplayer/user_progress_jwplayer.js
@@ -2,25 +2,39 @@
 $(document).ready(function() {
 	// record the fact that they started watching the video
 	jwplayer().onPlay(function() {
-    var params = 'value=1&data=play&upregid='+ Drupal.settings.user_progress.upregid;
-    Drupal.user_progress.ajax_call('set', 'value', params);
+		// only track the first time play is pressed
+    if (!Drupal.settings.user_progress.pressed_play) {
+      var params = 'value=1&data=video started&upregid='+ Drupal.settings.user_progress.upregid;
+      Drupal.user_progress.ajax_call('set', 'value', params);
+			// set true so that this can't happen again for this transaction
+		  Drupal.settings.user_progress.pressed_play = true;
+		}
   });
 	// flag that the user finished watching the video
 	jwplayer().onComplete(function() {
-    var params = 'value='+ jwplayer().getBuffer() +'&data=finished&upregid='+ Drupal.settings.user_progress.upregid;
+    var params = 'value=100&data=video finished&upregid='+ Drupal.settings.user_progress.upregid;
     Drupal.user_progress.ajax_call('set', 'value', params);
   });
 	// Determine if the user jumped ahead of back in the video
 	jwplayer().onSeek(function(event) {
 		var pos = event.offset - event.position;
+		var direction = '';
 		if (pos > 0) {
-			var direction = 'skip ahead';
+			direction = 'skip to '+ event.offset;
 		}
     else {
-			var direction = 'skip back';
+			direction = 'back to '+ event.offset;
 		}
-		var params = 'value='+ jwplayer().getBuffer() +'&data='+ direction +'&upregid='+ Drupal.settings.user_progress.upregid;
+		var params = 'value='+ event.position +'&data='+ direction +'&upregid='+ Drupal.settings.user_progress.upregid;
     Drupal.user_progress.ajax_call('set', 'value', params);
   });
-	// onTime -- % completion if it's not overkill to monitor, prolly is
+	// monitor and report the entire process
+	// this is an example of how to flood your database with meaningless data as this is the % left they have to watch of the video
+	/* jwplayer().onTime(function(event) {
+		var length = event.duration;
+		var current = event.position;
+		var params = 'value='+ Math.round(current/length * 100) +'&data=stamp&upregid='+ Drupal.settings.user_progress.upregid;
+    Drupal.user_progress.ajax_call('set', 'value', params);
+	});
+	*/
 });
